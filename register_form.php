8edb6d9edf63b3bff7bee22d4c1d74d2ea46b589
chrispharaoh1 <cpharaoh1@gmail.com>
2024-04-17 10:29:37 +0200

    PHP file for handling register created

---
+++ register_form.php
@@ -0,0 +1,37 @@
+<?php
+// Retrieve form data
+$data = json_decode(file_get_contents('php://input'), true);
+$name = $data['name'];
+$phoneNumber = $data['phoneNumber'];
+$email = $data['email'];
+$password = $data['password'];
+
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "food_store";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare SQL statement
+$stmt = $conn->prepare("INSERT INTO fcustomers (name,phone, email, password) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $name, $phoneNumber, $email, $password);
+
+// Execute SQL statement
+if ($stmt->execute() === TRUE) {
+    echo json_encode(array("message" => "Form data inserted successfully"));
+} else {
+    echo json_encode(array("error" => "Error inserting form data: " . $conn->error));
+}
+
+// Close prepared statement and database connection
+$stmt->close();
+$conn->close();
+?>
