403e00972a1fb33589bd09de34569a56ad8d5921
chrispharaoh1 <cpharaoh1@gmail.com>
2024-04-21 13:55:59 +0200

    Front page done

---
+++ js/optionsArrayHome.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to fetch menu options and generate HTML cards
+    function fetchMenuOptions() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'server/optionsArray.php', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Parse JSON response
+                var menuOptions = JSON.parse(xhr.responseText);
+
+                // Generate HTML cards
+                var menuOptionsHTML = '';
+                menuOptions.forEach(function(option) {
+                    menuOptionsHTML += '<div class="row justify-content-center">';
+                    menuOptionsHTML += '<div class="col-md-12 col-xl-10">';
+                    menuOptionsHTML += '<div class="card shadow-0 border rounded-3">';
+                    menuOptionsHTML += '<div class="card-body">';
+                    menuOptionsHTML += '<div class="row">';
+
+                    //Div for image
+                    menuOptionsHTML += '<div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">';
+                    menuOptionsHTML += '<div class="bg-image hover-zoom ripple rounded ripple-surface">';
+                        //image
+                        var imgUrl = "img/"+option['image_data'];
+                        menuOptionsHTML += '<img src="' + imgUrl + '" class="w-100">';
+                    menuOptionsHTML += '</div>';
+                    menuOptionsHTML += '</div>';
+
+                    //Div for main content
+                    menuOptionsHTML += '<div class="col-md-6 col-lg-6 col-xl-6">';
+                        //title
+                        menuOptionsHTML += '<h5>' +option['category'] + ', ' + option['option_name'] + '</h5>';
+
+                        //star rating
+                        menuOptionsHTML += '<div class="d-flex flex-row">';
+                        menuOptionsHTML += '<div class="text-danger mb-1 me-2">';
+                            menuOptionsHTML += '<i class="fa fa-star">' + '</i>';
+                            menuOptionsHTML += '<i class="fa fa-star">' + '</i>';
+                            menuOptionsHTML += '<i class="fa fa-star">' + '</i>';
+                            menuOptionsHTML += '<i class="fa fa-star">' + '</i>';
+                        menuOptionsHTML += '</div>';
+                        menuOptionsHTML += '</div>';
+
+                        //Contents description
+                        menuOptionsHTML += '<div class="mt-1 mb-0 text-muted small">';
+                            menuOptionsHTML += '<label>' +'IIngridients : ' + option['ingredients']+ '</label>';
+                        menuOptionsHTML += '</div>';
+                        menuOptionsHTML += '</div>';
+                        
+
+                        //Price and shipping
+                        menuOptionsHTML += '<div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">';
+                            menuOptionsHTML += '<div class="d-flex flex-row align-items-center mb-1">';
+                                menuOptionsHTML += '<h4 class="mb-1 me-1">' +'Price: $'  + option['price'] + '</4>';
+                            menuOptionsHTML += '</div>';
+                                 menuOptionsHTML += '<h6 class="text-success">' +'Free shipping' +'</h5>';
+
+                                 //button for buy and login
+                                 menuOptionsHTML += '<div class="d-flex flex-column mt-4">';
+                                    menuOptionsHTML += '<button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-sm" type="button" onclick="redirectToPay()">Buy</button>';
+                                 menuOptionsHTML += '</div>';
+
+                        menuOptionsHTML += '</div>';
+
+                    
+                    menuOptionsHTML += '</div>';
+                    menuOptionsHTML += '</div>';
+                    menuOptionsHTML += '</div>';
+                    menuOptionsHTML += '</div>';
+                    menuOptionsHTML += '</div>';
+
+                });
+
+                // Insert generated HTML into DOM
+                document.getElementById('menuItems').innerHTML = menuOptionsHTML;
+            }
+        };
+        xhr.send();
+    }
+
+    // Call fetchMenuOptions function to load menu options when the page loads
+    fetchMenuOptions();
+
+    // Function to redirect to pay.html with price and ingredients as query parameters
+ 
+});
+
+function redirectToPay() {
+ alert("You need to log in to buy this product!")
+}
---
+++ js/select_menu_home.js
@@ -0,0 +1,151 @@
+// Function to populate food categories from the database
+function populateFoodCategories() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'server/get_menu_options.php', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var categories = JSON.parse(xhr.responseText);
+                var foodCategorySelect = document.getElementById("foodCategory");
+                categories.forEach(category => {
+                    var option = document.createElement("option");
+                    option.value = category.menu_id; // Assuming menu_id is the value for categories
+                    option.textContent = category.menu_name; // Assuming menu_name is the name for categories
+                    foodCategorySelect.appendChild(option);
+                });
+            } else {
+                console.error('Failed to fetch food categories');
+            }
+        }
+    };
+    xhr.send();
+}
+
+// Function to populate food options based on selected category
+function populateFoodOptions() {
+    var selectedCategory = document.getElementById("foodCategory").value;
+    var foodTypeSelect = document.getElementById("foodType");
+    var labelSelectDiv = document.getElementById("labelSelect");
+
+    // Clear previous options
+    foodTypeSelect.innerHTML = "<option value=''>Select</option>";
+    labelSelectDiv.innerHTML = "";
+
+    if (selectedCategory !== "") {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'server/options.php?menuId=' + selectedCategory, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var options = JSON.parse(xhr.responseText);
+                    options.forEach(option => {
+                        var optionElement = document.createElement("option");
+                        optionElement.value = option.option_name;
+                        optionElement.textContent = option.option_name;
+                        foodTypeSelect.appendChild(optionElement);
+                    });
+                    // Enable food type select
+                    foodTypeSelect.style.display = 'block';
+                    foodTypeSelect.disabled = false;
+
+                      //title text for select
+                      var selectLabe = document.createElement("label");
+                      selectLabe.textContent = "Select Food Type:";
+                      labelSelectDiv.appendChild(selectLabe);
+
+                } else {
+                    console.error('Failed to fetch food options');
+                }
+            }
+        };
+        xhr.send();
+    } else {
+        // Disable food type select if no category selected
+        foodTypeSelect.disabled = true;
+        // Hide food type select if no category selected
+        foodTypeSelect.style.display = 'none';
+    }
+}
+
+
+
+// Function to display food pictures
+function displayFoodPictures() {
+    var selectedCategory = document.getElementById("foodCategory").value;
+    var selectedFood = document.getElementById("foodType").value;
+    var foodPicturesDiv = document.getElementById("foodPictures"); 
+    var foodDiv = document.getElementById("foodT");
+    var priceDiv = document.getElementById("price");
+    var headingDiv = document.getElementById("heading");
+    var qtyDiv = document.getElementById("qty");
+    var buyDiv = document.getElementById("buy");
+    
+
+    // Clear previous elements
+    foodPicturesDiv.innerHTML = ""; 
+    foodDiv.innerHTML = "";
+    priceDiv.innerHTML = "";
+    headingDiv.innerHTML = "";
+    qtyDiv.innerHTML = "";
+    buyDiv.innerHTML = "";
+    
+
+    if (selectedFood !== "") {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'server/get_food_image.php?category=' + selectedCategory + '&food=' + selectedFood, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var imageData = JSON.parse(xhr.responseText);
+
+                    //creating a picture tag
+                        var img = document.createElement("img");
+                        //Adding image source from the database
+                        img.src = imageData.imageUrl;
+                        img.alt = selectedFood;
+                        img.className = "col-md-4";
+                        //appending the image to the picture id to be displayed
+                        foodPicturesDiv.appendChild(img); 
+                        
+                        //heading
+                        var h4 = document.createElement("h4");
+                        h4.textContent = selectedFood;
+                        headingDiv.appendChild(h4);
+
+                        //Food type
+                        var label = document.createElement("label");
+                        label.textContent = " "+selectedFood;
+                        foodDiv.appendChild(label);
+
+                         //Price
+                         var price = document.createElement("label");
+                         price.textContent = imageData.priceInDollars;
+                         priceDiv.appendChild(price);
+
+                        //displaying the block containing the peice and other infor
+                        document.getElementById("product-info").style.display = 'block';
+
+                } else {
+                    console.error('Failed to fetch food image');
+                }
+            }
+        };
+        xhr.send();
+
+        // var img = document.createElement("img");
+        // // img.src = imageData.image_url;
+        // img.src = "FRONT.PNG";
+        // img.alt = selectedFood;
+        // img.className = "col-md-4";
+        // foodPicturesDiv.appendChild(img);
+    }
+}
+
+
+// Event listeners
+document.getElementById("foodCategory").addEventListener("change", populateFoodOptions);
+document.getElementById("foodType").addEventListener("change", displayFoodPictures);
+
+
+// Call the function to populate food categories when the page loads
+populateFoodCategories();
